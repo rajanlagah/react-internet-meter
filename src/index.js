6f07592cadb80d6a3fe4fee12a0f82b4215adba8
rajanlagah <rajanlagah@gmail.com>
2022-03-29 20:30:01 +0530

    ⚡️ Changed interval to timeout

--- src/index.js
+++ src/index.js
@@ -3,96 +3,92 @@ import styles from './styles.module.css'
 
 import { Alert } from './components'
 
-export const ReactInternetSpeedMeter = ({ 
-  txtMainHeading="Opps..." ,
-  outputType="alert",
+export const ReactInternetSpeedMeter = ({
+  txtMainHeading = 'Opps...',
+  outputType = 'alert',
   customClassName,
-  pingInterval=30000,
-  thresholdUnit='megabyte',
-  threshold=7,
-  txtSubHeading="Diconnected from internet",
-  imageUrl="https://res.cloudinary.com/dcwxsms2l/image/upload/v1610376487/pexels-ivan-samkov-6291574_bzqgps.jpg",
-  downloadSize="1781287", //bytes
-  callbackFunctionOnNetworkDown=()=>console.log("No callback on Network Down"),
-  callbackFunctionOnNetworkTest=()=>console.log("No callback On Network test"),
-  
+  pingInterval = 30000,
+  thresholdUnit = 'megabyte',
+  threshold = 7,
+  txtSubHeading = 'Diconnected from internet',
+  imageUrl,
+  downloadSize, //bytes
+  callbackFunctionOnNetworkDown = () => console.log('No callback on Network Down'),
+  callbackFunctionOnNetworkTest = () => console.log('No callback On Network test'),
+  callbackFunctionOnError = () => console.log('error in downloading image')
 }) => {
-
-  const [ isNetworkDown,setisNetworkDown ] = useState(false)
+  const [isNetworkDown, setisNetworkDown] = useState(false)
   let intervalFun
 
-  window.addEventListener('offline', ()=> setisNetworkDown(true));
-  window.addEventListener('online', ()=> setisNetworkDown(false));
-  
-  const startInterval = () => {
-    return window.setInterval(MeasureConnectionSpeed, pingInterval);
+  window.addEventListener('offline', () => setisNetworkDown(true));
+  window.addEventListener('online', () => setisNetworkDown(false));
+
+  const startCalculating = () => {
+    return window.setTimeout(MeasureConnectionSpeed, pingInterval);
   }
 
-  useEffect(()=> {
-    intervalFun = startInterval()
+  useEffect(() => {
+    intervalFun = startCalculating()
     return () => window.clearInterval(intervalFun)
-  },[])
-
-
-  
+  }, [])
 
   const MeasureConnectionSpeed = () => {
-      var startTime, endTime;
-      var download = new Image();
-      startTime = (new Date()).getTime();
+    var startTime, endTime;
+    var download = new Image();
+    startTime = (new Date()).getTime();
 
-      var cacheBuster = "?nnn=" + startTime;
-      download.src = imageUrl + cacheBuster;
+    var cacheBuster = '?nnn=' + startTime;
+    download.src = imageUrl + cacheBuster;
 
-      download.onload = function (d) {
-          endTime = (new Date()).getTime();
-          showResults(startTime , endTime);
-      }
-      
-      download.onerror = function (err, msg) {
-          console.log('err',err)
-          window.clearInterval(intervalFun)
+    download.onload = function (d) {
+      endTime = (new Date()).getTime();
+      showResults(startTime, endTime);
+    }
 
-      }
+    download.onerror = function (err, msg) {
+      console.log('err', err)
+      callbackFunctionOnError(err)
+      window.clearInterval(intervalFun)
+    }
+    startCalculating()
   }
 
-  const showResults = (startTime , endTime) => {
-      const duration = (endTime - startTime) / 1000;
+  const showResults = (startTime, endTime) => {
+    const duration = (endTime - startTime) / 1000;
 
-      const bitsLoaded = downloadSize * 8;
-      const speedBps = (bitsLoaded / duration).toFixed(2);
-      const speedKbps = (speedBps / 1024).toFixed(2);
-      const speedMbps = (speedKbps / 1024).toFixed(2);
-  
-      setNetworStatus(speedBps,speedKbps,speedMbps)
+    const bitsLoaded = downloadSize * 8;
+    const speedBps = (bitsLoaded / duration).toFixed(2);
+    const speedKbps = (speedBps / 1024).toFixed(2);
+    const speedMbps = (speedKbps / 1024).toFixed(2);
+
+    setNetworStatus(speedBps, speedKbps, speedMbps)
   }
 
-  const setNetworStatus = (speedBps,speedKbps,speedMbps) => {
-    console.log("HERE",speedMbps)
-    if( thresholdUnit === 'byte'){
-      if(speedBps < threshold){
+  const setNetworStatus = (speedBps, speedKbps, speedMbps) => {
+    if (thresholdUnit === 'byte') {
+      if (speedBps < threshold) {
         setisNetworkDown(true)
         callbackFunctionOnNetworkDown(speedBps)
-      }else {
+      } else {
         setisNetworkDown(false)
       }
       callbackFunctionOnNetworkTest(speedBps)
     }
-    else if( thresholdUnit === 'kilobyte'){
-      if(speedKbps < threshold){
+    else if (thresholdUnit === 'kilobyte') {
+      if (speedKbps < threshold) {
         setisNetworkDown(true)
         callbackFunctionOnNetworkDown(speedKbps)
-      }else {
+      } else {
         setisNetworkDown(false)
       }
       callbackFunctionOnNetworkTest(speedKbps)
 
     }
-    else if( thresholdUnit === 'megabyte'){
-      if(speedMbps < threshold){
+    else if (thresholdUnit === 'megabyte') {
+      if (speedMbps < threshold) {
         setisNetworkDown(true)
         callbackFunctionOnNetworkDown(speedMbps)
-      }else {
+      } else {
         setisNetworkDown(false)
       }
       callbackFunctionOnNetworkTest(speedMbps)
@@ -103,16 +99,16 @@ export const ReactInternetSpeedMeter = ({
     }
   }
 
-  if(isNetworkDown){
-    if(outputType == 'alert'){
+  if (isNetworkDown) {
+    if (outputType === 'alert') {
       return <Alert
-                alertStyles={styles} 
-                customClassName={customClassName}
-                txtMainHeading={txtMainHeading} 
-                txtSubHeading={txtSubHeading } />
+        alertStyles={styles}
+        customClassName={customClassName}
+        txtMainHeading={txtMainHeading}
+        txtSubHeading={txtSubHeading} />
     }
 
-    return <div/>
+    return <div />
   }
-  return <div/>
+  return <div />
 }
